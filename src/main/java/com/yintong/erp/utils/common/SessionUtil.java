576c72fe4f146b26962f1d6b2e4f90e4270d3a70
lucifer-Chan <lucifer.chan@foxmail.com>
2018-05-05 22:05:16 +0800

    完善会话辅助功能

--- src/main/java/com/yintong/erp/utils/common/SessionUtil.java
+++ src/main/java/com/yintong/erp/utils/common/SessionUtil.java
@@ -1,13 +1,76 @@
 package com.yintong.erp.utils.common;
 
+import com.yintong.erp.domain.basis.security.ErpEmployee;
+import com.yintong.erp.exception.SessionExpiryException;
+import com.yintong.erp.security.EmployeeDetails;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author lucifer.chan
  * @create 2018-05-05 下午3:15
  * session辅助类
  **/
 public class SessionUtil {
+    private static final String KEY_PREFIX = SessionUtil.class.getName()+ "_";
+
+    public static <T> T get(String key){
+        return get(null, key);
+    }
+
+    public static <T> void set(String key, T value){
+        set(null, key, value);
+    }
+
+    public static <T> T remove(String key){
+        return remove(null, key);
+    }
+
     public static Long getCurrentUserId(){
-        //TODO
-        return -1L;
+        ErpEmployee currentUser = getCurrentUser();
+        SessionExpiryException.notNull(currentUser, "会话过期！未获取到当前用户");
+        return currentUser.getId();
+    }
+
+    private static ErpEmployee getCurrentUser(){
+        EmployeeDetails employeeDetails = getEmployeeDetails();
+        SessionExpiryException.notNull(employeeDetails, "会话过期！未获取到当前用户明细");
+        return employeeDetails.getEmployee();
+    }
+
+    private static EmployeeDetails getEmployeeDetails(){
+        SecurityContext securityContext =
+                (SecurityContext)SpringUtil.getRequest().getSession().getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        SessionExpiryException.notNull(securityContext, "会话过期！");
+        return (EmployeeDetails)securityContext.getAuthentication().getPrincipal();
+    }
+
+    @SuppressWarnings(value = "unchecked")
+    public static <T> T get(HttpServletRequest request, String key){
+        request = request(request);
+        if(null == request)
+            return null;
+        return (T)request.getSession().getAttribute(KEY_PREFIX + key);
+    }
+
+    public static <T> void set(HttpServletRequest request, String key, T value){
+        request = request(request);
+        if(null != request)
+            request.getSession().setAttribute(KEY_PREFIX + key, value);
+    }
+
+    public static <T> T remove(HttpServletRequest request, String key){
+        request = request(request);
+        if(null == request)
+            return null;
+        T t = get(request, key);
+        request.getSession().removeAttribute(key);
+        return t;
+    }
+
+    private static HttpServletRequest request(HttpServletRequest request){
+        return null != request ? request : SpringUtil.getRequest();
     }
 }
