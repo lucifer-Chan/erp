7a1f53753114920f9c6d09d643e242e883d238bb
lucifer-Chan <lucifer.chan@foxmail.com>
2018-05-06 03:06:16 +0800

    漏写了@Configuration

--- src/main/java/com/yintong/erp/config/AppConfig.java
+++ src/main/java/com/yintong/erp/config/AppConfig.java
@@ -3,6 +3,7 @@ package com.yintong.erp.config;
 import com.yintong.erp.utils.common.SimpleCache;
 import com.yintong.erp.utils.common.SpringUtil;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @author lucifer.chan
  * @create 2018-05-05 下午3:53
  **/
+@Configuration
 public class AppConfig implements WebMvcConfigurer {
     @Bean
     public SpringUtil springUtil(){
