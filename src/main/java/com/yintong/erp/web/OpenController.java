b38c988e58eaaea542bf9dfb24f96dee3e71a910
lucifer-Chan <lucifer.chan@foxmail.com>
2018-05-06 03:05:03 +0800

    开放性的接口-非登陆用户可访问

---
+++ src/main/java/com/yintong/erp/web/OpenController.java
@@ -0,0 +1,14 @@
+package com.yintong.erp.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lucifer.chan
+ * @create 2018-05-05 下午11:43
+ * 开放性的接口-非登陆用户可访问
+ **/
+@RestController
+@RequestMapping("open")
+public class OpenController {
+}
