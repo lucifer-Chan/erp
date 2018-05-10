7b5eff3091c0cb1a2738cdf070a210306c2ac827
lucifer-Chan <lucifer.chan@foxmail.com>
2018-05-11 00:31:05 +0800

    删除没用的

--- src/main/java/com/yintong/erp/domain/basis/barCode/ErpBaseBarCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yintong.erp.domain.basis.barCode;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/**
- * Created by jianqiang on 2018/5/10 0010.
- */
-public interface ErpBaseBarCodeRepository extends JpaRepository<ErpBaseBarCode, Long> {
-}
