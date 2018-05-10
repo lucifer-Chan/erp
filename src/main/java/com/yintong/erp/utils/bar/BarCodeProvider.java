e4a2f711920a4ef150f98bd25c9720bedbb2fa4d
lucifer-Chan <lucifer.chan@foxmail.com>
2018-05-10 15:30:48 +0800

    考虑父类有@BarCode注解，采取列表形式排序继承关系Class，取最近的一个@BarCode

--- src/main/java/com/yintong/erp/utils/bar/BarCodeProvider.java
+++ src/main/java/com/yintong/erp/utils/bar/BarCodeProvider.java
@@ -35,46 +35,47 @@ public class BarCodeProvider implements PreInsertEventListener, PreUpdateEventLi
         return false;
     }
 
-
     private void onPreCommit(AbstractPreDatabaseOperationEvent event, Object[] state){
         Serializable id = event.getId();
         Object entity = event.getEntity();
-        Class<?> clazz = entity.getClass();
-        if(clazz.isAnnotationPresent(BarCode.class)) {
-            //前缀
-            BAR_CODE_PREFIX prefix = clazz.getAnnotation(BarCode.class).value();
-            List<Field> columns = ReflectUtil.getFieldsByAnnotation(entity, BarCodeColumn.class);
-            //拥有@BarCodeIndex的字段，排序
-            List<Field> indexes = ReflectUtil.getFieldsByAnnotation(entity, BarCodeIndex.class).stream()
-                    .sorted(Comparator.comparingInt(f -> f.getAnnotation(BarCodeIndex.class).value()))
-                    .collect(Collectors.toList());
-            Assert.isTrue(columns.size() == 1, "实体类必须有且只有一个拥有@BarCodeColumn的字段！");
-            //供存储的字段
-            Field targetField = columns.get(0);
-            Assert.isTrue(targetField.getGenericType().equals(String.class), "@BarCodeColumn标注的字段必须为String类型");
+        Class<?> clazz = ReflectUtil.getClassesUntilRoot(entity).stream()
+                .filter(c -> c.isAnnotationPresent(BarCode.class))
+                .findFirst().orElse(null);
+        if(Objects.isNull(clazz)) return;
 
-            String[] propertyNames = event.getPersister().getEntityMetamodel().getPropertyNames();
+        //前缀
+        BAR_CODE_PREFIX prefix = clazz.getAnnotation(BarCode.class).value();
+        List<Field> columns = ReflectUtil.getFieldsByAnnotation(entity, BarCodeColumn.class);
+        //拥有@BarCodeIndex的字段，排序
+        List<Field> indexes = ReflectUtil.getFieldsByAnnotation(entity, BarCodeIndex.class).stream()
+                .sorted(Comparator.comparingInt(f -> f.getAnnotation(BarCodeIndex.class).value()))
+                .collect(Collectors.toList());
+        Assert.isTrue(columns.size() == 1, "实体类必须有且只有一个拥有@BarCodeColumn的字段！");
+        //供存储的字段
+        Field targetField = columns.get(0);
+        Assert.isTrue(targetField.getGenericType().equals(String.class), "@BarCodeColumn标注的字段必须为String类型");
+
+        String[] propertyNames = event.getPersister().getEntityMetamodel().getPropertyNames();
 //            Object[] state = event.getState();
 
-            //1-前缀+id
-            StringBuilder barCode = new StringBuilder(prefix.name()).append(id);
+        //1-前缀+id
+        StringBuilder barCode = new StringBuilder(prefix.name()).append(id);
 
-            //2-预条码+indexes
-            for (Field field : indexes){
-                Object value = Unchecked.biFunction(ReflectUtil::getValue).apply(field, entity);
-                barCode.append((Objects.isNull(value) || !StringUtils.hasLength(value.toString()))? "K" : value);
-            }
+        //2-预条码+indexes
+        for (Field field : indexes){
+            Object value = Unchecked.biFunction(ReflectUtil::getValue).apply(field, entity);
+            barCode.append((Objects.isNull(value) || !StringUtils.hasLength(value.toString()))? "K" : value);
+        }
 
-            for (int i = 0; i < propertyNames.length ; i ++) {
-                if (targetField.getName().equals(propertyNames[i])){
-                    state[i] = barCode.toString();
-                }
+        for (int i = 0; i < propertyNames.length ; i ++) {
+            if (targetField.getName().equals(propertyNames[i])){
+                state[i] = barCode.toString();
             }
-            try{
-                ReflectUtil.setValue(targetField, entity, barCode.toString());
-            } catch (Exception e){
-                e.printStackTrace();
-            }
+        }
+        try{
+            ReflectUtil.setValue(targetField, entity, barCode.toString());
+        } catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
