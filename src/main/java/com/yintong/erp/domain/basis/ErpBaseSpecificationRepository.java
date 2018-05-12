f687e951a3eda98ace2077bcc3d4149000c4edc7
976594640@qq.com <976594640@qq.com>
2018-05-12 18:50:24 +0800

    task#git1.增加基础信息dao

--- src/main/java/com/yintong/erp/domain/basis/ErpBaseSpecificationRepository.java
+++ src/main/java/com/yintong/erp/domain/basis/ErpBaseSpecificationRepository.java
@@ -2,8 +2,13 @@ package com.yintong.erp.domain.basis;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * Created by jianqiang on 2018/5/12.
  */
 public interface ErpBaseSpecificationRepository extends JpaRepository<ErpBaseSpecification, Long> {
+
+    List<ErpBaseSpecification> findAll();
+
 }
