fee724040892536307109310d1c6d1f77c2e3cdf
976594640@qq.com <976594640@qq.com>
2018-05-12 21:23:53 +0800

    task#清空单元测试类

--- src/test/java/com/yintong/erp/ErpApplicationTests.java
+++ src/test/java/com/yintong/erp/ErpApplicationTests.java
@@ -1,11 +1,10 @@
 package com.yintong.erp;
 
-import com.yintong.erp.domain.basis.warehouse.ErpBaseWarehouse;
-import com.yintong.erp.domain.basis.warehouse.ErpBaseWarehouseRepository;
+
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -13,8 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ErpApplicationTests {
 
-	@Autowired
-	private ErpBaseWarehouseRepository erpBaseWarehouseRepository;
+
 
 	@Test
 	public void contextLoads() {
@@ -23,9 +21,7 @@ public class ErpApplicationTests {
 	@Ignore
 	@Test
 	public void entityTest1(){
-		ErpBaseWarehouse erpBaseWarehouse = new ErpBaseWarehouse();
-		erpBaseWarehouse.setAssetNo("00001");
-		erpBaseWarehouseRepository.save(erpBaseWarehouse);
+
 
 	}
 
