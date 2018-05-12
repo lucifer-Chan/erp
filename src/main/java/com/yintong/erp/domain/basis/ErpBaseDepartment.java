5146ce0bbad24da2f91cdf4be8933abe456dc84f
lucifer-Chan <lucifer.chan@foxmail.com>
2018-05-13 00:54:29 +0800

    部门树型结构

--- src/main/java/com/yintong/erp/domain/basis/ErpBaseDepartment.java
+++ src/main/java/com/yintong/erp/domain/basis/ErpBaseDepartment.java
@@ -1,9 +1,11 @@
 package com.yintong.erp.domain.basis;
 
 import com.yintong.erp.utils.base.BaseEntity;
+import com.yintong.erp.utils.transform.IgnoreIfNull;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.List;
 
 /**
  * @author lucifer.chan
@@ -16,12 +18,18 @@ public class ErpBaseDepartment extends BaseEntity{
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(columnDefinition = "varchar(10) comment '编号'")
+    @Column(columnDefinition = "varchar(20) comment '编号'")
     private String code;
 
     @Column(columnDefinition = "varchar(40) comment '名称'")
     private String name;
 
+    @Column(columnDefinition = "bigint(20) comment '父节点id'")
+    private Long parentId;
+
     @Column(columnDefinition = "varchar(100) comment '说明'")
     private String description;
+
+    @Transient @IgnoreIfNull
+    private List<ErpBaseDepartment> children;
 }
