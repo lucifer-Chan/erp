3ddce278359cabb142170d0eee04388697d13673
976594640@qq.com <976594640@qq.com>
2018-05-12 20:47:33 +0800

    task#git1.仓库

--- src/main/java/com/yintong/erp/domain/basis/ErpBaseWarehouse.java
+++ src/main/java/com/yintong/erp/domain/basis/ErpBaseWarehouse.java
@@ -1,11 +1,9 @@
 package com.yintong.erp.domain.basis;
 
+import com.yintong.erp.utils.bar.BarCode;
 import com.yintong.erp.utils.base.BaseEntity;
 import com.yintong.erp.utils.base.BaseEntityWithBarCode;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -16,9 +14,11 @@ import javax.persistence.Id;
  * Created by jianqiang on 2018/5/10 0010.
  * 仓位
  */
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
+@Setter
+@Getter
 @Builder
 @Entity
 public class ErpBaseWarehouse extends BaseEntityWithBarCode {
@@ -30,6 +30,7 @@ public class ErpBaseWarehouse extends BaseEntityWithBarCode {
     private String warehouseNo;
     @Column(columnDefinition = "varchar(20) comment '仓库名'")
     private String warehouseName;
+    @BarCode
     @Column(columnDefinition = "varchar(20) comment '仓位类别编码'")
     private String warehouseTypeCode;
     @Column(columnDefinition = "varchar(12) comment '库存最大量'")
