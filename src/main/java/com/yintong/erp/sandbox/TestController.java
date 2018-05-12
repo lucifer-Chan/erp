3b3fbb48196bb80f6af779d34cc2e51ca44ff0f0
lucifer-Chan <lucifer.chan@foxmail.com>
2018-05-13 00:53:19 +0800

    测试controller

---
+++ src/main/java/com/yintong/erp/sandbox/TestController.java
@@ -0,0 +1,43 @@
+package com.yintong.erp.sandbox;
+
+import com.yintong.erp.service.CategoryService;
+import com.yintong.erp.service.DepartmentService;
+import com.yintong.erp.utils.base.BaseResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lucifer.chan
+ * @create 2018-05-12 下午6:33
+ * 测试用
+ **/
+@RestController
+@RequestMapping("test")
+public class TestController {
+    @Autowired CategoryService categoryService;
+    @Autowired DepartmentService departmentService;
+
+    @GetMapping("category/{code}")
+    public BaseResult getTree(@PathVariable  String code){
+        return new BaseResult().addList("tree", categoryService.tree(code));
+    }
+
+    @GetMapping("category")
+    public BaseResult getTree(){
+        return new BaseResult().addList("tree", categoryService.tree());
+    }
+
+    @GetMapping("department/{id}")
+    public BaseResult department(@PathVariable  Long id){
+        return new BaseResult().addList("tree", departmentService.tree(id));
+    }
+
+    @GetMapping("department")
+    public BaseResult department2(){
+        return new BaseResult().addList("tree", departmentService.tree());
+    }
+}
+
